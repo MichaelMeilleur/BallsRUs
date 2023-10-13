45c32e6c1cfc0294488a740378ea68400ef2e14d
Olivier Charron <2089149@etu.cegepsth.qc.ca>
2023-10-13 18:02:15 -0400

    Modification de certaines entitées et ajout du seed.

--- BallsRUs/Context/ApplicationDbContext.cs
+++ BallsRUs/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using BallsRUs.Entities;
+﻿using BallsRUs.Data;
+using BallsRUs.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -18,8 +19,18 @@ namespace BallsRUs.Context
                 .HasMany(e => e.Categories)
                 .WithMany(e => e.Products);
 
+            modelBuilder.Entity<ProductCategory>()
+                .HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+            modelBuilder.Entity<Product>()
+                .HasMany(product => product.Categories)
+                .WithMany(category => category.Products)
+                .UsingEntity<ProductCategory>(
+                    j => j.HasOne(pc => pc.Category).WithMany().HasForeignKey(pc => pc.CategoryId),
+                    j => j.HasOne(pc => pc.Product).WithMany().HasForeignKey(pc => pc.ProductId));
+
             base.OnModelCreating(modelBuilder);
-            //modelBuilder.Seed();
+            modelBuilder.Seed();
         }
     }
 }
