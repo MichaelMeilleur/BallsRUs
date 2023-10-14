5abcf2b42ef892273a9eca57378650a77e9bba0c
Michael Meilleur <1375593@etu.cegepsth.qc.ca>
2023-10-13 22:18:43 -0400

    Programmation de la page login et de la logique du LogIn

---
+++ BallsRUs/Models/Account/LogInVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BallsRUs.Models.Account
+{
+    public class LogInVM
+    {
+        [Display(Name = "Nom d'utilisateur")]
+        public string NomUtilisateur { get; set; }
+
+        [Display(Name = "Mot de Passe")]
+        [DataType(DataType.Password)]
+        public string MotDePasse { get; set; }
+    }
+}
