72376042073d801a073e0a04d34aa5a1399f0618
Michael Meilleur <1375593@etu.cegepsth.qc.ca>
2023-10-14 13:23:26 -0400

    Changements dans la bd et du fonctionnement du register

--- BallsRUs/Models/Account/LogInVM.cs
+++ BallsRUs/Models/Account/LogInVM.cs
@@ -4,10 +4,8 @@ namespace BallsRUs.Models.Account
 {
     public class LogInVM
     {
-        [Display(Name = "Nom d'utilisateur")]
         public string NomUtilisateur { get; set; }
 
-        [Display(Name = "Mot de Passe")]
         [DataType(DataType.Password)]
         public string MotDePasse { get; set; }
     }
