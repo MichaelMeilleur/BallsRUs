368c8399e18ec7249c5bcc42521cbab67424e479
Michael Meilleur <1375593@etu.cegepsth.qc.ca>
2023-10-13 22:18:43 -0400

    Programmation de la page login et de la logique du LogIn

---
+++ BallsRUs/Models/Account/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace BallsRUs.Models.Account
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Nom d'utilisateur")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Password confirmation")]
+        [Compare(nameof(Password))]
+        public string PasswordConfirmation { get; set; }
+    }
+    public class RegisterVMValidator : AbstractValidator<RegisterVM>
+    {
+        public RegisterVMValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("Le nom d'utilisateur est obligatoire.")
+                .Matches("^[^\\s]{5,20}$").WithMessage("Le nom d'utilisateur doit contenir entre 5 et 20 caractères sans espaces.");
+        }
+    }
+}
