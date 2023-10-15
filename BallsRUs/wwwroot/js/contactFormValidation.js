3fa26776a1d386195404065988726a9d0599be49
Xavier Côté-Daviau <xaviercotedaviau@gmail.com>
2023-10-15 14:55:32 -0400

    Validation du courriel fonctionnelle

---
+++ BallsRUs/wwwroot/js/contactFormValidation.js
@@ -0,0 +1,14 @@
+﻿function validateContactFormSubmit() {
+    let x = document.forms["contactForm"]["email"].value;
+    if (x == "") {
+        alert("Vous devez entrer votre courriel.");
+        return false;
+    }
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(x)) {
+        alert("Entrez un courriel valide.");
+        return false;
+    }
+
+    return true;
+}
