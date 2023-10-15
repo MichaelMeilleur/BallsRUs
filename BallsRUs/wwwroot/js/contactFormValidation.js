5a4389daf6904f472ae5e406b2acee37c9682529
Xavier Côté-Daviau <xaviercotedaviau@gmail.com>
2023-10-15 15:08:14 -0400

    Validation complète

--- BallsRUs/wwwroot/js/contactFormValidation.js
+++ BallsRUs/wwwroot/js/contactFormValidation.js
@@ -1,12 +1,32 @@
 ﻿function validateContactFormSubmit() {
+    // Valide le courriel
     let x = document.forms["contactForm"]["email"].value;
     if (x == "") {
         alert("Vous devez entrer votre courriel.");
         return false;
     }
-    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regex.test(x)) {
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexEmail.test(x)) {
         alert("Entrez un courriel valide.");
+        return false;
+    }
+
+    // Valide le nom
+    let y = document.forms["contactForm"]["subject"].value;
+    if (y == "") {
+        alert("Vous devez entrer votre nom.");
+        return false;
+    }
+    let regexNom = /^.{2,20}$/;
+    if (!regexNom.text(y)) {
+        alert("Entrez un nom valide.");
+        return false;
+    }
+
+    // Valide le message
+    let z = document.forms["contactForm"]["body"].value;
+    if (y == "") {
+        alert("Vous devez entrer un message.");
         return false;
     }
 
