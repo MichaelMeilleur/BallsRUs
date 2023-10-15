aabfe6dacf6bb3f8d652ca03915b72c8455a7985
Olivier Charron <2089149@etu.cegepsth.qc.ca>
2023-10-15 17:05:43 -0400

    Ajout des fonctionnalités d'Admin pour ajouter, archiver et consulter les produits

---
+++ BallsRUs/Models/Admin/AdminCreateProductVM.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BallsRUs.Models.Admin
+{
+    public class AdminCreateProductVM
+    {
+        [Display(Name = "SKU")]
+        public string? SKU { get; set; }
+
+        [Display(Name = "Nom")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Marque")]
+        public string? Brand { get; set; }
+
+        [Display(Name = "Modèle")]
+        public string? Model { get; set; }
+
+        [Display(Name = "Fichier image")]
+        public IFormFile? Image { get; set; }
+
+        [Display(Name = "Description courte")]
+        public string? ShortDescription { get; set; }
+
+        [Display(Name = "Description complète")]
+        public string? FullDescription { get; set; }
+
+        [Display(Name = "Poids en grammes")]
+        public int? WeightInGrams { get; set; }
+
+        [Display(Name = "Taille")]
+        public string? Size { get; set; }
+
+        [Display(Name = "Quantités")]
+        public int? Quantity { get; set; }
+
+        [Display(Name = "Prix")]
+        public decimal? RetailPrice { get; set; }
+
+        [Display(Name = "Prix en rabais (si en rabais)")]
+        public decimal? DiscountedPrice { get; set; }
+    }
+}
