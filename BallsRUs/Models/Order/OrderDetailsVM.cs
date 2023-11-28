45b8d6ae19151c0df94d1b38f4997cf0cd6345b9
Mahdi Ellili <2003186@etu.cegepsth.qc.ca>
2023-11-28 13:47:54 -0500

    manageorders

---
+++ BallsRUs/Models/Order/OrderDetailsVM.cs
@@ -0,0 +1,43 @@
+﻿using BallsRUs.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BallsRUs.Models.Order
+{
+    public class OrderDetailsVM
+    {
+        [Display(Name = "Id")]
+        public Guid Id { get; set; }
+        [Display(Name = "Order#")]
+        public string? Number { get; set; }
+        [Display(Name = "Status")]
+        public OrderStatus Status { get; set; } = OrderStatus.Opened;
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        [Display(Name = "Email")]
+        public string? EmailAddress { get; set; }
+        [Display(Name = "Phone number")]
+        public string? PhoneNumber { get; set; }
+        [Display(Name = "Quantity")]
+        public int ProductQuantity { get; set; }
+        [Display(Name = "Product price")]
+        public decimal? ProductsCost { get; set; }
+        [Display(Name = "Shipping fees")]
+        public decimal? ShippingCost { get; set; }
+        [Display(Name = "Sub total")]
+        public decimal? SubTotal { get; set; }
+        [Display(Name = "Taxes")]
+        public decimal? Taxes { get; set; }
+        [Display(Name = "Total")]
+        public decimal? Total { get; set; }
+        [Display(Name = "Creation date")]
+        public DateTime CreationDate { get; set; }
+        [Display(Name = "Confirmation date")]
+        public DateTime? ConfirmationDate { get; set; }
+        [Display(Name = "Purchased on")]
+        public DateTime? PaymentDate { get; set; }
+        [Display(Name = "Modified on")]
+        public DateTime? ModificationDate { get; set; }
+    }
+}
