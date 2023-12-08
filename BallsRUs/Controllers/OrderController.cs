363f71d02a5c29ca89e3cc888ee7a0b65426177f
Olivier Charron <2089149@etu.cegepsth.qc.ca>
2023-12-08 16:44:21 -0500

    N'ajoute plus les items au panier après la cancelation d'une commande.

--- BallsRUs/Controllers/OrderController.cs
+++ BallsRUs/Controllers/OrderController.cs
@@ -13,11 +13,11 @@ namespace BallsRUs.Controllers
         private readonly ApplicationDbContext _context;
 
         public OrderController(UserManager<User> userManager, ApplicationDbContext context)
-        {   
+        {
             _userManager = userManager;
             _context = context;
         }
-      
+
         public IActionResult OrderDetails(Guid orderId)
         {
             ViewBag.OrderId = orderId;
@@ -61,71 +61,12 @@ namespace BallsRUs.Controllers
             {
                 List<OrderItem> orderToCancelItems = _context.OrderItems.Where(oi => oi.OrderId == orderToCancel.Id).ToList();
 
-                ShoppingCart? shoppingCart = _context.ShoppingCarts.FirstOrDefault(sc => sc.UserId == orderToCancel.UserId);
-
-                if (shoppingCart is not null)
+                foreach (OrderItem item in orderToCancelItems)
                 {
-                    foreach (OrderItem item in orderToCancelItems)
-                    {
-                        Product? product = _context.Products.Find(item.ProductId);
+                    Product? product = _context.Products.Find(item.ProductId);
 
-                        if (product is not null)
-                        {
-                            product.Quantity += item.Quantity;
-
-                            ShoppingCartItem? sci = _context.ShoppingCartItems
-                                .FirstOrDefault(i => i.ShoppingCartId == shoppingCart.Id && i.ProductId == product.Id);
-
-                            if (sci is not null)
-                            {
-                                sci.Quantity += item.Quantity;
-                            }
-                            else
-                            {
-                                ShoppingCartItem? newSci = new ShoppingCartItem()
-                                {
-                                    Quantity = item.Quantity,
-                                    CreationDate = DateTime.UtcNow,
-                                    ShoppingCartId = shoppingCart.Id,
-                                    ProductId = product.Id
-                                };
-
-                                _context.ShoppingCartItems.Add(newSci);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    ShoppingCart newSC = new ShoppingCart
-                    {
-                        Id = Guid.NewGuid(),
-                        ProductsQuantity = 0,
-                        CreationDate = DateTime.UtcNow,
-                        UserId = orderToCancel.UserId
-                    };
-
-                    _context.ShoppingCarts.Add(newSC);
-
-                    foreach (var item in orderToCancelItems)
-                    {
-                        Product? product = _context.Products.Find(item.ProductId);
-
-                        if (product is not null)
-                        {
-                            product.Quantity += item.Quantity;
-
-                            ShoppingCartItem? newSci = new ShoppingCartItem()
-                            {
-                                Quantity = item.Quantity,
-                                CreationDate = DateTime.UtcNow,
-                                ShoppingCartId = newSC.Id,
-                                ProductId = product.Id
-                            };
-
-                            _context.ShoppingCartItems.Add(newSci);
-                        }
-                    }
+                    if (product is not null)
+                        product.Quantity += item.Quantity;
                 }
 
                 orderToCancel.Status = OrderStatus.Canceled;
