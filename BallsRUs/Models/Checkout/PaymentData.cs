49a0b5e1ca4ea3198380b830b00f4ac41d673813
ocharron <o_charron@outlook.com>
2023-12-11 14:33:59 -0500

    Ajout du courriel

--- BallsRUs/Models/Checkout/PaymentData.cs
+++ BallsRUs/Models/Checkout/PaymentData.cs
@@ -3,6 +3,7 @@
     public class PaymentData
     {
         public string? Name { get; set; }
+        public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
